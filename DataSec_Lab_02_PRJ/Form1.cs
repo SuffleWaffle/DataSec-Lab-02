2f03759a4165a5781b1435361ca1e14987596b11
SuffleWaffle <74103889+SuffleWaffle@users.noreply.github.com>
2023-01-20 16:25:26 +0200

    DataSec Lab 03 init

---
+++ DataSec_Lab_02_PRJ/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSec_Lab_02_PRJ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
