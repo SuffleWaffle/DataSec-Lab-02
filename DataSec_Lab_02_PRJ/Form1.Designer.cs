9977e3852634b9ad9b7566468125dd8d92d77752
SuffleWaffle <74103889+SuffleWaffle@users.noreply.github.com>
2023-01-20 22:00:29 +0200

    DataSec Lab 02 Fixed Commit

--- DataSec_Lab_02_PRJ/Form1.Designer.cs
+++ DataSec_Lab_02_PRJ/Form1.Designer.cs
@@ -3,38 +3,370 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
                 components.Dispose();
             }
-
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.c16Lb = new System.Windows.Forms.Label();
+            this.c10Lb = new System.Windows.Forms.Label();
+            this.c2Lb = new System.Windows.Forms.Label();
+            this.key16Lb = new System.Windows.Forms.Label();
+            this.key10Lb = new System.Windows.Forms.Label();
+            this.key2Lb = new System.Windows.Forms.Label();
+            this.p16Lb = new System.Windows.Forms.Label();
+            this.p10Lb = new System.Windows.Forms.Label();
+            this.p2Lb = new System.Windows.Forms.Label();
+            this.decodeLb = new System.Windows.Forms.Label();
+            this.encodeLb = new System.Windows.Forms.Label();
+            this.pDecodeTB = new System.Windows.Forms.TextBox();
+            this.keyDecodeTB = new System.Windows.Forms.TextBox();
+            this.cDecodeTB = new System.Windows.Forms.TextBox();
+            this.pDecodeLb = new System.Windows.Forms.Label();
+            this.keyDecodeLb = new System.Windows.Forms.Label();
+            this.cDecodeLb = new System.Windows.Forms.Label();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.decodeBtn = new System.Windows.Forms.Button();
+            this.cEncodeTB = new System.Windows.Forms.TextBox();
+            this.keyEncodeTB = new System.Windows.Forms.TextBox();
+            this.pEncodeTB = new System.Windows.Forms.TextBox();
+            this.cEncodeLb = new System.Windows.Forms.Label();
+            this.keyEncodeLb = new System.Windows.Forms.Label();
+            this.pEncodeLb = new System.Windows.Forms.Label();
+            this.formatGB = new System.Windows.Forms.GroupBox();
+            this.encodeBtn = new System.Windows.Forms.Button();
+            this.formatGB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // c16Lb
+            // 
+            this.c16Lb.AutoSize = true;
+            this.c16Lb.Location = new System.Drawing.Point(243, 65);
+            this.c16Lb.Name = "c16Lb";
+            this.c16Lb.Size = new System.Drawing.Size(35, 13);
+            this.c16Lb.TabIndex = 51;
+            this.c16Lb.Text = "C 16 -";
+            // 
+            // c10Lb
+            // 
+            this.c10Lb.AutoSize = true;
+            this.c10Lb.Location = new System.Drawing.Point(149, 65);
+            this.c10Lb.Name = "c10Lb";
+            this.c10Lb.Size = new System.Drawing.Size(35, 13);
+            this.c10Lb.TabIndex = 50;
+            this.c10Lb.Text = "C 10 -";
+            // 
+            // c2Lb
+            // 
+            this.c2Lb.AutoSize = true;
+            this.c2Lb.Location = new System.Drawing.Point(12, 65);
+            this.c2Lb.Name = "c2Lb";
+            this.c2Lb.Size = new System.Drawing.Size(29, 13);
+            this.c2Lb.TabIndex = 49;
+            this.c2Lb.Text = "C 2 -";
+            // 
+            // key16Lb
+            // 
+            this.key16Lb.AutoSize = true;
+            this.key16Lb.Location = new System.Drawing.Point(243, 42);
+            this.key16Lb.Name = "key16Lb";
+            this.key16Lb.Size = new System.Drawing.Size(46, 13);
+            this.key16Lb.TabIndex = 48;
+            this.key16Lb.Text = "Key 16 -";
+            // 
+            // key10Lb
+            // 
+            this.key10Lb.AutoSize = true;
+            this.key10Lb.Location = new System.Drawing.Point(149, 42);
+            this.key10Lb.Name = "key10Lb";
+            this.key10Lb.Size = new System.Drawing.Size(46, 13);
+            this.key10Lb.TabIndex = 47;
+            this.key10Lb.Text = "Key 10 -";
+            // 
+            // key2Lb
+            // 
+            this.key2Lb.AutoSize = true;
+            this.key2Lb.Location = new System.Drawing.Point(12, 42);
+            this.key2Lb.Name = "key2Lb";
+            this.key2Lb.Size = new System.Drawing.Size(40, 13);
+            this.key2Lb.TabIndex = 46;
+            this.key2Lb.Text = "Key 2 -";
+            // 
+            // p16Lb
+            // 
+            this.p16Lb.AutoSize = true;
+            this.p16Lb.Location = new System.Drawing.Point(243, 20);
+            this.p16Lb.Name = "p16Lb";
+            this.p16Lb.Size = new System.Drawing.Size(35, 13);
+            this.p16Lb.TabIndex = 45;
+            this.p16Lb.Text = "P 16 -";
+            // 
+            // p10Lb
+            // 
+            this.p10Lb.AutoSize = true;
+            this.p10Lb.Location = new System.Drawing.Point(149, 20);
+            this.p10Lb.Name = "p10Lb";
+            this.p10Lb.Size = new System.Drawing.Size(35, 13);
+            this.p10Lb.TabIndex = 44;
+            this.p10Lb.Text = "P 10 -";
+            // 
+            // p2Lb
+            // 
+            this.p2Lb.AutoSize = true;
+            this.p2Lb.Location = new System.Drawing.Point(12, 20);
+            this.p2Lb.Name = "p2Lb";
+            this.p2Lb.Size = new System.Drawing.Size(29, 13);
+            this.p2Lb.TabIndex = 43;
+            this.p2Lb.Text = "P 2 -";
+            // 
+            // decodeLb
+            // 
+            this.decodeLb.AutoSize = true;
+            this.decodeLb.Location = new System.Drawing.Point(77, 108);
+            this.decodeLb.Name = "decodeLb";
+            this.decodeLb.Size = new System.Drawing.Size(45, 13);
+            this.decodeLb.TabIndex = 42;
+            this.decodeLb.Text = "Decode";
+            // 
+            // encodeLb
+            // 
+            this.encodeLb.AutoSize = true;
+            this.encodeLb.Location = new System.Drawing.Point(77, 9);
+            this.encodeLb.Name = "encodeLb";
+            this.encodeLb.Size = new System.Drawing.Size(44, 13);
+            this.encodeLb.TabIndex = 41;
+            this.encodeLb.Text = "Encode";
+            // 
+            // pDecodeTB
+            // 
+            this.pDecodeTB.Location = new System.Drawing.Point(47, 175);
+            this.pDecodeTB.Name = "pDecodeTB";
+            this.pDecodeTB.ReadOnly = true;
+            this.pDecodeTB.Size = new System.Drawing.Size(100, 20);
+            this.pDecodeTB.TabIndex = 40;
+            // 
+            // keyDecodeTB
+            // 
+            this.keyDecodeTB.Location = new System.Drawing.Point(47, 150);
+            this.keyDecodeTB.Name = "keyDecodeTB";
+            this.keyDecodeTB.Size = new System.Drawing.Size(100, 20);
+            this.keyDecodeTB.TabIndex = 39;
+            this.keyDecodeTB.TextChanged += new System.EventHandler(this.keyDecodeTB_TextChanged);
+            // 
+            // cDecodeTB
+            // 
+            this.cDecodeTB.Location = new System.Drawing.Point(47, 124);
+            this.cDecodeTB.Name = "cDecodeTB";
+            this.cDecodeTB.Size = new System.Drawing.Size(100, 20);
+            this.cDecodeTB.TabIndex = 38;
+            this.cDecodeTB.TextChanged += new System.EventHandler(this.cDecodeTB_TextChanged);
+            // 
+            // pDecodeLb
+            // 
+            this.pDecodeLb.AutoSize = true;
+            this.pDecodeLb.Location = new System.Drawing.Point(10, 178);
+            this.pDecodeLb.Name = "pDecodeLb";
+            this.pDecodeLb.Size = new System.Drawing.Size(23, 13);
+            this.pDecodeLb.TabIndex = 37;
+            this.pDecodeLb.Text = "P =";
+            // 
+            // keyDecodeLb
+            // 
+            this.keyDecodeLb.AutoSize = true;
+            this.keyDecodeLb.Location = new System.Drawing.Point(10, 150);
+            this.keyDecodeLb.Name = "keyDecodeLb";
+            this.keyDecodeLb.Size = new System.Drawing.Size(34, 13);
+            this.keyDecodeLb.TabIndex = 36;
+            this.keyDecodeLb.Text = "Key =";
+            // 
+            // cDecodeLb
+            // 
+            this.cDecodeLb.AutoSize = true;
+            this.cDecodeLb.Location = new System.Drawing.Point(10, 127);
+            this.cDecodeLb.Name = "cDecodeLb";
+            this.cDecodeLb.Size = new System.Drawing.Size(23, 13);
+            this.cDecodeLb.TabIndex = 35;
+            this.cDecodeLb.Text = "C =";
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(383, 124);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(107, 65);
+            this.clearBtn.TabIndex = 33;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // decodeBtn
+            // 
+            this.decodeBtn.Location = new System.Drawing.Point(270, 124);
+            this.decodeBtn.Name = "decodeBtn";
+            this.decodeBtn.Size = new System.Drawing.Size(107, 65);
+            this.decodeBtn.TabIndex = 32;
+            this.decodeBtn.Text = "Decode";
+            this.decodeBtn.UseVisualStyleBackColor = true;
+            this.decodeBtn.Click += new System.EventHandler(this.decodeBtn_Click);
+            // 
+            // cEncodeTB
+            // 
+            this.cEncodeTB.Location = new System.Drawing.Point(51, 76);
+            this.cEncodeTB.Name = "cEncodeTB";
+            this.cEncodeTB.ReadOnly = true;
+            this.cEncodeTB.Size = new System.Drawing.Size(100, 20);
+            this.cEncodeTB.TabIndex = 31;
+            // 
+            // keyEncodeTB
+            // 
+            this.keyEncodeTB.Location = new System.Drawing.Point(51, 51);
+            this.keyEncodeTB.Name = "keyEncodeTB";
+            this.keyEncodeTB.Size = new System.Drawing.Size(100, 20);
+            this.keyEncodeTB.TabIndex = 30;
+            this.keyEncodeTB.TextChanged += new System.EventHandler(this.keyEncodeTB_TextChanged);
+            // 
+            // pEncodeTB
+            // 
+            this.pEncodeTB.Location = new System.Drawing.Point(51, 25);
+            this.pEncodeTB.Name = "pEncodeTB";
+            this.pEncodeTB.Size = new System.Drawing.Size(100, 20);
+            this.pEncodeTB.TabIndex = 29;
+            this.pEncodeTB.TextChanged += new System.EventHandler(this.pEncodeTB_TextChanged);
+            // 
+            // cEncodeLb
+            // 
+            this.cEncodeLb.AutoSize = true;
+            this.cEncodeLb.Location = new System.Drawing.Point(11, 79);
+            this.cEncodeLb.Name = "cEncodeLb";
+            this.cEncodeLb.Size = new System.Drawing.Size(23, 13);
+            this.cEncodeLb.TabIndex = 28;
+            this.cEncodeLb.Text = "C =";
+            // 
+            // keyEncodeLb
+            // 
+            this.keyEncodeLb.AutoSize = true;
+            this.keyEncodeLb.Location = new System.Drawing.Point(10, 54);
+            this.keyEncodeLb.Name = "keyEncodeLb";
+            this.keyEncodeLb.Size = new System.Drawing.Size(34, 13);
+            this.keyEncodeLb.TabIndex = 27;
+            this.keyEncodeLb.Text = "Key =";
+            // 
+            // pEncodeLb
+            // 
+            this.pEncodeLb.AutoSize = true;
+            this.pEncodeLb.Location = new System.Drawing.Point(11, 28);
+            this.pEncodeLb.Name = "pEncodeLb";
+            this.pEncodeLb.Size = new System.Drawing.Size(23, 13);
+            this.pEncodeLb.TabIndex = 26;
+            this.pEncodeLb.Text = "P =";
+            // 
+            // formatGB
+            // 
+            this.formatGB.Controls.Add(this.c16Lb);
+            this.formatGB.Controls.Add(this.p2Lb);
+            this.formatGB.Controls.Add(this.c10Lb);
+            this.formatGB.Controls.Add(this.p10Lb);
+            this.formatGB.Controls.Add(this.c2Lb);
+            this.formatGB.Controls.Add(this.p16Lb);
+            this.formatGB.Controls.Add(this.key16Lb);
+            this.formatGB.Controls.Add(this.key2Lb);
+            this.formatGB.Controls.Add(this.key10Lb);
+            this.formatGB.Location = new System.Drawing.Point(157, 12);
+            this.formatGB.Name = "formatGB";
+            this.formatGB.Size = new System.Drawing.Size(333, 91);
+            this.formatGB.TabIndex = 52;
+            this.formatGB.TabStop = false;
+            this.formatGB.Text = "Formatted Values";
+            // 
+            // encodeBtn
+            // 
+            this.encodeBtn.Location = new System.Drawing.Point(157, 124);
+            this.encodeBtn.Name = "encodeBtn";
+            this.encodeBtn.Size = new System.Drawing.Size(107, 65);
+            this.encodeBtn.TabIndex = 53;
+            this.encodeBtn.Text = "Encode";
+            this.encodeBtn.UseVisualStyleBackColor = true;
+            this.encodeBtn.Click += new System.EventHandler(this.encodeBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(505, 206);
+            this.Controls.Add(this.encodeBtn);
+            this.Controls.Add(this.decodeLb);
+            this.Controls.Add(this.encodeLb);
+            this.Controls.Add(this.pDecodeTB);
+            this.Controls.Add(this.keyDecodeTB);
+            this.Controls.Add(this.cDecodeTB);
+            this.Controls.Add(this.pDecodeLb);
+            this.Controls.Add(this.keyDecodeLb);
+            this.Controls.Add(this.cDecodeLb);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.decodeBtn);
+            this.Controls.Add(this.cEncodeTB);
+            this.Controls.Add(this.keyEncodeTB);
+            this.Controls.Add(this.pEncodeTB);
+            this.Controls.Add(this.cEncodeLb);
+            this.Controls.Add(this.keyEncodeLb);
+            this.Controls.Add(this.pEncodeLb);
+            this.Controls.Add(this.formatGB);
+            this.Name = "Form1";
+            this.Text = "XOR Encoder/Decoder";
+            this.formatGB.ResumeLayout(false);
+            this.formatGB.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label c16Lb;
+        private System.Windows.Forms.Label c10Lb;
+        private System.Windows.Forms.Label c2Lb;
+        private System.Windows.Forms.Label key16Lb;
+        private System.Windows.Forms.Label key10Lb;
+        private System.Windows.Forms.Label key2Lb;
+        private System.Windows.Forms.Label p16Lb;
+        private System.Windows.Forms.Label p10Lb;
+        private System.Windows.Forms.Label p2Lb;
+        private System.Windows.Forms.Label decodeLb;
+        private System.Windows.Forms.Label encodeLb;
+        private System.Windows.Forms.TextBox pDecodeTB;
+        private System.Windows.Forms.TextBox keyDecodeTB;
+        private System.Windows.Forms.TextBox cDecodeTB;
+        private System.Windows.Forms.Label pDecodeLb;
+        private System.Windows.Forms.Label keyDecodeLb;
+        private System.Windows.Forms.Label cDecodeLb;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button decodeBtn;
+        private System.Windows.Forms.TextBox cEncodeTB;
+        private System.Windows.Forms.TextBox keyEncodeTB;
+        private System.Windows.Forms.TextBox pEncodeTB;
+        private System.Windows.Forms.Label cEncodeLb;
+        private System.Windows.Forms.Label keyEncodeLb;
+        private System.Windows.Forms.Label pEncodeLb;
+        private System.Windows.Forms.GroupBox formatGB;
+        private System.Windows.Forms.Button encodeBtn;
     }
 }
--- DataSec_Lab_02_PRJ/Form1.cs
+++ DataSec_Lab_02_PRJ/Form1.cs
@@ -15,6 +15,105 @@ namespace DataSec_Lab_02_PRJ
         public Form1()
         {
             InitializeComponent();
+            pEncodeTB.Text = "0x";
+            keyEncodeTB.Text = "0x";
+            cDecodeTB.Text = "0x";
+            keyDecodeTB.Text = "0x";
+        }
+
+        private void encodeBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int pInt = Convert.ToInt32(pEncodeTB.Text, 16);
+                int keyInt = Convert.ToInt32(keyEncodeTB.Text, 16);
+                int cInt = pInt ^ keyInt;
+                cEncodeTB.Text = "0x" + Convert.ToString(cInt, 16).ToUpper();
+                p2Lb.Text = "P 2 - " + Convert.ToString(pInt, 2);
+                p10Lb.Text = "P 10 - " + Convert.ToString(pInt, 10);
+                p16Lb.Text = "P 16 - " + Convert.ToString(pInt, 16).ToUpper();
+                c2Lb.Text = "C 2 - " + Convert.ToString(cInt, 2);
+                c10Lb.Text = "C 10 - " + Convert.ToString(cInt, 10);
+                c16Lb.Text = "C 16 - " + Convert.ToString(cInt, 16).ToUpper();
+                key2Lb.Text = "Key 2 - " + Convert.ToString(keyInt, 2);
+                key10Lb.Text = "Key 10 - " + Convert.ToString(keyInt, 10);
+                key16Lb.Text = "Key 16 - " + Convert.ToString(keyInt, 16).ToUpper();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("One of the values is too big!");
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Wrong HEX value format!");
+            }
+        }
+
+        private void decodeBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int cInt = Convert.ToInt32(cDecodeTB.Text, 16);
+                int keyInt = Convert.ToInt32(keyDecodeTB.Text, 16);
+                int pInt = cInt ^ keyInt;
+                pDecodeTB.Text = "0x" + Convert.ToString(pInt, 16).ToUpper();
+                p2Lb.Text = "P 2 - " + Convert.ToString(pInt, 2);
+                p10Lb.Text = "P 10 - " + Convert.ToString(pInt, 10);
+                p16Lb.Text = "P 16 - " + Convert.ToString(pInt, 16).ToUpper();
+                c2Lb.Text = "C 2 - " + Convert.ToString(cInt, 2);
+                c10Lb.Text = "C 10 - " + Convert.ToString(cInt, 10);
+                c16Lb.Text = "C 16 - " + Convert.ToString(cInt, 16).ToUpper();
+                key2Lb.Text = "Key 2 - " + Convert.ToString(keyInt, 2);
+                key10Lb.Text = "Key 10 - " + Convert.ToString(keyInt, 10);
+                key16Lb.Text = "Key 16 - " + Convert.ToString(keyInt, 16).ToUpper();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("One of the values is too big!");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Wrong HEX value format!");
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            pEncodeTB.Text = "0x";
+            keyEncodeTB.Text = "0x";
+            cEncodeTB.Text = "";
+            pDecodeTB.Text = "";
+            keyDecodeTB.Text = "0x";
+            cDecodeTB.Text = "0x";
+            p2Lb.Text = "P 2 -";
+            p10Lb.Text = "P 10 -";
+            p16Lb.Text = "P 16 -";
+            key2Lb.Text = "Key 2 -";
+            key10Lb.Text = "Key 10 -";
+            key16Lb.Text = "Key 16 -";
+            c2Lb.Text = "C 2 -";
+            c10Lb.Text = "C 10 -";
+            c16Lb.Text = "C 16 -";
+        }
+
+        private void pEncodeTB_TextChanged(object sender, EventArgs e)
+        {
+            if (pEncodeTB.Text.Length < 2) { pEncodeTB.Text = "0x"; pEncodeTB.SelectionStart = 2; }
+        }
+
+        private void keyEncodeTB_TextChanged(object sender, EventArgs e)
+        {
+            if (keyEncodeTB.Text.Length < 2) { keyEncodeTB.Text = "0x"; keyEncodeTB.SelectionStart = 2; }
+        }
+
+        private void cDecodeTB_TextChanged(object sender, EventArgs e)
+        {
+            if (cDecodeTB.Text.Length < 2) { cDecodeTB.Text = "0x"; cDecodeTB.SelectionStart = 2; }
+        }
+
+        private void keyDecodeTB_TextChanged(object sender, EventArgs e)
+        {
+            if (keyDecodeTB.Text.Length < 2) { keyDecodeTB.Text = "0x"; keyDecodeTB.SelectionStart = 2; }
         }
     }
 }
